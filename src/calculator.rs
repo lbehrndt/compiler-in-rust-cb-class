8cc1d9ff745ad805443e2de71942a38b0341df7a
Leon <94090091+lbehrndt@users.noreply.github.com>
2024-04-30 11:40:13 +0200

    Sheet1/task: interpreter implementation (#5)
    * successfully implement calculator
    
    * finish calculator implementation with docs and comments
    
    * comment out div operation for gh action test purposes
    
    * update .yml
    
    * remove os's
    
    * uncomment div method
    
    * refactor: create single function for binary operation
    
    * bug: add variable assignment to solution
    
    -- fixed
    
    * update comment on visitor
    
    * remove comment from visitor
    
    * Refactor/update GitHub actions (#7)
    
    * add pr template
    
    * update test run action
    
    * move pr template to own dir
    
    * update .yml
    
    * remove set-output as it is deprecated
    
    * test action
    
    * add new action for changed files
    
    * add condition to test run
    
    * test regex
    
    * fix if statement
    
    * cahnge to eng name
    
    * remove test
    
    * remove rs file check
    
    * add rust file check from action
    
    * only run tests on src files
    
    * test gh action
    
    * run test in any scenario
    
    * only run on pr
    
    * add or template
    
    * remove pr tempalte
    
    * Workflow: add pr template (#8)
    
    * Create pull_request_template.md
    
    * move pr template
    
    * Refactor/update GitHub actions (#7)
    
    * add pr template
    
    * update test run action
    
    * move pr template to own dir
    
    * update .yml
    
    * remove set-output as it is deprecated
    
    * test action
    
    * add new action for changed files
    
    * add condition to test run
    
    * test regex
    
    * fix if statement
    
    * cahnge to eng name
    
    * remove test
    
    * remove rs file check
    
    * add rust file check from action
    
    * only run tests on src files
    
    * test gh action
    
    * run test in any scenario
    
    * only run on pr
    
    * add or template
    
    * remove pr tempalte
    
    * Sheet1/task: parser-implementation (#2)
    
    * successful parser implementation
    
    * remove: TODO comment
    
    * chore: add comments and clean up
    
    * add more comments
    
    * update comment on parse_tree
    
    * refactor parse int
    
    * add comment
    
    * sheet1/task: pretty print implementation (#1)
    
    * successful pretty print implementation
    
    * add linebreak
    
    * doc: add comments and clean up
    
    * test
    
    * *bugfix* printer didn't show variable in operations after assignment
    
    * remove doc example
    
    * remove example in comment

--- src/calculator.rs
+++ src/calculator.rs
@@ -15,6 +15,8 @@
 //! println!("Final result: {}", result); // prints 7
 //! ```
 
+use std::collections::HashMap;
+
 use crate::parse_tree::*;
 
 /// `Calculator` is a struct designed to evaluate parsed arithmetic expressions.
@@ -27,22 +29,104 @@ use crate::parse_tree::*;
 /// let result = calculator.calc(&root);
 /// println!("The result of the calculation is: {}", result);
 /// # }
-/// ```
-#[derive(Default)]
+/// ``````
 pub struct Calculator {
-	// TODO: eventuell notwendige Attribute aufnehmen
+    /// The solution of the calculation.
+    solution: i64,
+    /// A HashMap to store variable assignments.
+    variables: HashMap<char, i64>,
+}
+
+impl Default for Calculator {
+    /// Creates a new `Calculator` instance with default values.
+    fn default() -> Self {
+        Self {
+            solution: 0,
+            variables: HashMap::with_capacity(26), // Initialize HashMap with capacity 26 for lowercase alphabet variables
+        }
+    }
 }
 
 impl Calculator {
-	/// Evaluates the entire parse tree starting from a [`Root`] and returns the
-	/// result of the last expression evaluated.
-	pub fn calc(&mut self, _t: &Root) -> i64 {
-		todo!("Ergebnis durch Ablaufen des Baums bestimmen")
-	}
+    /// Evaluates the entire parse tree starting from a [`Root`] and returns the
+    /// result of the last expression evaluated.
+    ///
+    /// ## Arguments
+    ///
+    /// * `_t` - The root of the parse tree.
+    ///
+    /// ## Returns
+    ///
+    /// The result of the evaluation.
+    pub fn calc(&mut self, _t: &Root) -> i64 {
+        self.visit_root(_t);
+        self.solution
+    }
+
+    /// Perform a given binary expression and add it to te solution.
+    fn perform_binary_operation(&mut self, lhs: &Expr, rhs: &Expr, bin_op: fn(i64, i64) -> i64) {
+        self.visit_expr(lhs);
+        let lhs_val = self.solution;
+        self.visit_expr(rhs);
+        let rhs_val = self.solution;
+
+        self.solution = bin_op(lhs_val, rhs_val)
+    }
 }
 
 impl Visitor for Calculator {
-	// TODO: relevante Methoden überschreiben
+    /// Visits the root of the parse tree.
+    fn visit_root(&mut self, root: &Root) {
+        for stmt in root.stmt_list.iter() {
+            self.visit_stmt(stmt);
+        }
+    }
+
+    /// Visits a statement in the parse tree.
+    fn visit_stmt(&mut self, stmt: &Stmt) {
+        match stmt {
+            Stmt::Expr(ref e) => self.visit_expr(e),
+            Stmt::Set(v, e) => {
+                self.visit_expr(e);
+                self.variables.insert(*v, self.solution); // declare variable as hashmap value
+                self.solution = 0; // after setting the variable we don't want to add it to any operation
+            }
+        }
+    }
+
+    /// Visits an expression in the parse tree.
+    ///
+    /// This method recursively evaluates the expression by visiting its sub-expressions
+    /// and updating the solution accordingly.
+    fn visit_expr(&mut self, expr: &Expr) {
+        match expr {
+            Expr::Int(i) => self.solution = *i,
+            Expr::Var(v) => {
+                if let Some(val) = self.variables.get(v) {
+                    self.solution = *val;
+                } else {
+                    panic!("Variable '{}' is not defined", v);
+                }
+            }
+            Expr::Add(lhs, rhs) => {
+                self.perform_binary_operation(lhs, rhs, |lhs_val, rhs_val| lhs_val + rhs_val)
+            }
+            Expr::Sub(lhs, rhs) => {
+                self.perform_binary_operation(lhs, rhs, |lhs_val, rhs_val| lhs_val - rhs_val)
+            }
+            Expr::Mul(lhs, rhs) => {
+                self.perform_binary_operation(lhs, rhs, |lhs_val, rhs_val| lhs_val * rhs_val)
+            }
+            Expr::Div(lhs, rhs) => {
+                if let Expr::Int(rhs_value) = **rhs {
+                    if rhs_value == 0 {
+                        panic!("attempt to divide by zero.")
+                    }
+                }
+                self.perform_binary_operation(lhs, rhs, |lhs_val, rhs_val| lhs_val / rhs_val)
+            }
+        }
+    }
 }
 
 // unit-tests
